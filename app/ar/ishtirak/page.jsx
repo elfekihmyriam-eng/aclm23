6a68eda842da6fe7ea32f2e960681a533d4246e7
Myriam Fekih <elfekihmyriam@gmail.com>
2026-01-20 19:24:15 -0500

    Create new ishtirak route (clean JSX)

---
+++ app/ar/ishtirak/page.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function IshtirakPage() {
+  return (
+    <main style={{ padding: "120px", textAlign: "center" }}>
+      <h1>الاشتراك</h1>
+      <p>صفحة الاشتراك قيد الإعداد</p>
+    </main>
+  );
+}
