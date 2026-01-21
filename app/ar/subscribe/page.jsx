1a4fbaf681d9e3555175e3facfa3e3174330ce05
Myriam Fekih <elfekihmyriam@gmail.com>
2026-01-20 19:18:17 -0500

    Recreate subscribe page (clean JSX)

---
+++ app/ar/subscribe/page.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function SubscribePage() {
+  return (
+    <main style={{ padding: "120px", textAlign: "center" }}>
+      <h1>صفحة الاشتراك</h1>
+      <p>الصفحة قيد الإعداد</p>
+    </main>
+  );
+}
